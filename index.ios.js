90cd1b6e1a80a1df348c1061f5ee70a8c528c8dc
Wenson Tsai <wensontsai@gmail.com>
2015-10-15 17:29:39 -0400

    thinking thoughts randomizing

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,5 @@
 /**
- * DeepThoughts Template
+ * DeepThoughts App v 0.2
  */
 'use strict';
 
@@ -12,32 +12,56 @@ var {
   TouchableHighlight,
 } = React;
 
-var MOCKED_DATA = [
-  {title: 'Deep Thought #1', content: "If there's no 'there' there, where is it and what's there?"},
-];
-
-var REQUEST_URL = "http://dressler.local/restapi_test/wp-json/wp/v2/posts/";
+// The URL for the `posts` endpoint provided by WP JSON API
+var REQUEST_URL = 'http://dressler.local/restapi_test/wp-json/wp/v2/posts/?filter[orderby]=rand&filter[per_page]=1';
 
 var DeepThoughts = React.createClass({
   getInitialState: function() {
     return {
-      thought: MOCKED_DATA[0],
+      //thought is initially set to null so that the loading message shows
+      thought: null,
     };
   },
-  componentDidMount: function(){
+  // Automatically called by react when this component has finished mounting.
+  componentDidMount: function() {
     this.fetchData();
   },
-  fetchData: function(){
+  // This is where the magic happens! Fetches the data from our API and updates the application state.
+  fetchData: function() {
+    this.setState({
+      // we'll also set thought to null when loading new thoughts so that the loading message shows
+      thought: null,
+    });
     fetch(REQUEST_URL)
-      .then( (response) => response.json() )
-      .then( (responseData) => {
+      .then((response) => response.json())
+      .then((responseData) => {
+        // this.setState() will cause the new data to be applied to the UI that is created by the `render` function below
         this.setState({
-          thought: { title: responseData[0].title.rendered, content: responseData[0].content.plaintext },
+          thought: { title: responseData[0].title.rendered, content: responseData[0].content.plaintext }
         });
       })
       .done();
   },
+  // instead of immediately rendering the template, we now check if there is data in the 'thought' variable
+  // and render a loading view if it's empty, or the 'thought' template if there is data
   render: function() {
+    if ( !this.state.thought ) {
+      return this.renderLoadingView();
+    }
+    return this.renderThought();
+  },
+  // the loading view template just shows the message "Thinking thoughts..."
+  renderLoadingView: function() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Thinking thoughts...
+        </Text>
+      </View>
+    );
+  },
+  // this is the original render function, now renamed to renderThought, which will render our main template 
+  renderThought: function() {
     return (
       <View style={styles.container}>
         <View style={styles.textContainer}>
@@ -52,6 +76,7 @@ var DeepThoughts = React.createClass({
           <TouchableHighlight
             style={styles.button}
             underlayColor='#ccc'
+            onPress={this.fetchData}
           >
             <Text style={styles.buttonText}>Hmmmmm...</Text>
           </TouchableHighlight>
